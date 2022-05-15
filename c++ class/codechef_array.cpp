5c83156d20b65a08214dbcf0ef4107b7c2b7d5e7
amol050 <105576002+amol050@users.noreply.github.com>
2022-05-15 10:31:44 +0530

    to_make_better_code
    To_make_better_code

--- c++ class/codechef_array.cpp
+++ c++ class/codechef_array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
    int sum=0,n,s,p,i;
    int a[11] ={0}; // mtlb sare zero
-// done some changes for scm project
+// done some changes for scm projec
    for(int j=0; j<n; j++){
        cin>>p>>s;
      //  a[p]=s;
